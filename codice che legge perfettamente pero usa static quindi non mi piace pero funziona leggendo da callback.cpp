a636690644d0e0f7e32075f6fb58c2c1c443cfc9
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2024-11-01 16:03:05 +0100

    Create codice che legge perfettamente pero usa static quindi non mi piace pero funziona leggendo da callback.cpp

---
+++ codice che legge perfettamente pero usa static quindi non mi piace pero funziona leggendo da callback.cpp
@@ -0,0 +1,69 @@
+void example(String myString) {
+  Serial.println("ciao " + myString);
+};
+
+String listenToNewSerialData(void (*myCallback)(String)) {
+  static String receivedString = "";
+
+  while(Serial.available() > 0) {
+    const char thisChar    = Serial.read()   ;
+    const bool isEndOfLine = thisChar == '\n';
+
+    if(isEndOfLine) {
+      myCallback(receivedString);
+      digitalWrite(2, !digitalRead(2));
+      receivedString = "";
+      break;
+    }
+
+    receivedString += thisChar;
+  };
+
+  return "";
+};
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+};
+
+void loop() {
+  listenToNewSerialData(&example);
+};
+
+
+// void example(String myString) {
+//   Serial.println("ciao " + myString);  // Properly prints "ciao" with received string
+// }
+
+// String listenToNewSerialData(void (*myCallback)(String)) {
+//   String receivedString = "";  // Accumulate across multiple calls
+
+//   // Check if there's data available in the serial buffer
+//   while (Serial.available() > 0) {
+//     char thisChar = Serial.read();
+    
+//     // Check for newline character to determine the end of input
+//     if (thisChar == '\n') {
+//       myCallback(receivedString);       // Call the callback with the full string
+//       digitalWrite(2, !digitalRead(2)); // Toggle LED state
+//       String completedString = receivedString;
+//       receivedString = "";              // Clear buffer for the next message
+//       return completedString;
+//     }
+
+//     receivedString += thisChar; // Accumulate character by character
+//   }
+
+//   return "";
+// }
+
+// void setup() {
+//   pinMode(2, OUTPUT);
+//   Serial.begin(115200);
+// }
+
+// void loop() {
+//   listenToNewSerialData(&example);  // Pass `example` as the callback
+// }
+
