a60f3dae7c5712ced064b65190f43425db947d7a
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-11-18 23:19:16 +0100

    save

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -2,21 +2,17 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Replace 0x27 with your display's I2C address.
 
-byte smiley[8] = {
-  B00000,
-  B10001,
-  B00000,
-  B00000,
-  B10001,
-  B01110,
-  B00000,
-};
-
 void setup() {
+  lcd.begin(16,2);
   lcd.backlight();
-  lcd.createChar(0, smiley);
-  lcd.begin(16, 2);  
-  lcd.write(byte(0));
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, World!");
+  lcd.print("Hello, World!");
+
 }
 
-void loop() {}
+void loop() {
+  lcd.scrollDisplayLeft();
+  delay(200);
+}
+
