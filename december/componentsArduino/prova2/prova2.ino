27154602912044eae4884030fd93c1f226f07d18
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-11-19 00:32:25 +0100

    funziona e ricomincia da capo

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -2,33 +2,40 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // Replace 0x27 with your display's I2C address.
 
-byte progressoChars[5][8] = = {
-  {b10000, b11000, b11100, b11110, b11111},
-  {b10000, b11000, b11100, b11110, b11111},
-  {b10000, b11000, b11100, b11110, b11111},
-  {b10000, b11000, b11100, b11110, b11111},
-  {b10000, b11000, b11100, b11110, b11111}
+byte progressoChars[5][8] = {
+  {0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000, 0b10000},
+  {0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000, 0b11000},
+  {0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100, 0b11100},
+  {0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110, 0b11110},
+  {0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111, 0b11111}
 };
 
 int i=0;
+int k=0;
 
 void setup() {
   Serial.begin(9600);
 
-  for (int i = 0; i < 5; i++) {
-    lcd.createChar(i, progressoChars[i]);
+  for (int i = 1; i <= 5; i++) {
+    lcd.createChar(i, progressoChars[i-1]);
   };
 
   lcd.begin(16,2);
+  lcd.setCursor(0,0);
   lcd.backlight();
 }
 
 void loop() {
   i++;
-  const String myOutputString = String("ciao ") + String(i);
-  const int posizioneXcentro = (16-myOutputString.length())/2;
-  lcd.setCursor(posizioneXcentro, 0);
-  lcd.print(myOutputString);
-  delay(100);
+  k++;
+
+
+  lcd.write(byte(k));
+  Serial.println("k=" + String(k) + "\t i=" + String(i));
+  delay(500);
   lcd.clear();
+
+  if(i%5==0) {
+    k=0;
+  };
 }
