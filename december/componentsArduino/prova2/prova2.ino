71b261d32cf5ccc4ab54436bb0d7bcb1ae940367
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-01 17:02:25 +0100

    save

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -46,101 +46,104 @@ class laaDisplay {
   };
 
   struct configurations {
-
+    /*
+      RS:
+      - LOW   = CONFIG MODE
+      - HIGH  = WRITING MODE
+    */
+    void setConfigMode() { 
+      this->updatePinState(this->pins.rs, LOW);
+      this->sendCommand();
+    };
   }
 
-  // struct Mode {
-  //   struct Config {
-  //     void setConfigMode() {
-  //       /*
-  //         RS:
-  //         - LOW   = CONFIG MODE
-  //         - HIGH  = WRITING MODE
-  //       */
-  //       this->pins.rs.value = LOW;
+  struct Mode {
+    struct Config {
+      void setConfigMode() {
+        this->pins.rs.value = LOW;
 
-  //       this->sendCommand();
-  //     };
-  //     struct FunctionSet {
-  //       void setFunctionSet() {
-  //         /*
-  //           D5:
-  //           - HIGH = FUNCTION SET MODE
-  //         */
-  //         this->pins.d5.value = HIGH;
-  //       };
-  //       struct bitMode {
-  //         void setTo4() {
-  //           this->mode.config.setConfigMode();
-  //           this->mode.config.functionSet.setFunctionSet();
-  //           /*
-  //             D4:
-  //             - LOW  = 4BIT MODE
-  //             - HIGH = 8BIT MODE
-  //           */
-  //           this->pins.d4.value = LOW;
-  //         };
-  //         void setTo8() {
-  //           this->mode.config.setConfigMode();
-  //           this->mode.config.functionSet.setFunctionSet();
-  //           /*
-  //             D4:
-  //             - LOW  = 4BIT MODE
-  //             - HIGH = 8BIT MODE
-  //           */
-  //           this->pins.d4.value = HIGH;
-  //         };
-  //       } bitMode;
-  //       struct SetNumberOf {
-  //         struct lines {
-  //           void setTo1() {
-  //             this->mode.config.setConfigMode();
-  //             this->mode.config.functionSet.setFunctionSet();
-  //             /*
-  //               D3:
-  //               - LOW  = 1 LINE DISPLAY
-  //               - HIGH = 2 LINE DISPLAY
-  //             */
-  //             this->pins.d3.value = LOW;
-  //           };
-  //           void setTo2() {
-  //             this->mode.config.setConfigMode();
-  //             this->mode.config.functionSet.setFunctionSet();
-  //             /*
-  //               D3:
-  //               - LOW  = 1 LINE DISPLAY
-  //               - HIGH = 2 LINE DISPLAY
-  //             */
-  //             this->pins.d3.value = HIGH;
-  //           };
-  //         } lines;
-  //         struct Dots {
-  //           void mode5x8() {
-  //             this->mode.config.setConfigMode();
-  //             this->mode.config.functionSet.setFunctionSet();
-  //             /*
-  //               D2:
-  //               - LOW  = 5x8 DOTS MODE
-  //               - HIGH = 5x11 DOTS MODE
-  //             */
-  //             this->pins.d2.value = LOW;
-  //           };
-  //           void mode5x11() {
-  //             this->mode.config.setConfigMode();
-  //             this->mode.config.functionSet.setFunctionSet();
-  //             /*
-  //               D2:
-  //               - LOW  = 5x8 DOTS MODE
-  //               - HIGH = 5x11 DOTS MODE
-  //             */
-  //             this->pins.d2.value = HIGH;
-  //           };
-  //         } dots;
-  //       } setNumberOf;
-  //     } functionSet;
+        this->sendCommand();
+      };
+      struct FunctionSet {
+        void setFunctionSet() {
+          /*
+            D5:
+            - HIGH = FUNCTION SET MODE
+          */
+          this->pins.d5.value = HIGH;
+        };
+        struct bitMode {
+          void setTo4() {
+            this->mode.config.setConfigMode();
+            this->mode.config.functionSet.setFunctionSet();
+            /*
+              D4:
+              - LOW  = 4BIT MODE
+              - HIGH = 8BIT MODE
+            */
+            this->pins.d4.value = LOW;
+          };
+          void setTo8() {
+            this->mode.config.setConfigMode();
+            this->mode.config.functionSet.setFunctionSet();
+            /*
+              D4:
+              - LOW  = 4BIT MODE
+              - HIGH = 8BIT MODE
+            */
+            this->pins.d4.value = HIGH;
+          };
+        } bitMode;
+        struct SetNumberOf {
+          struct lines {
+            void setTo1() {
+              this->mode.config.setConfigMode();
+              this->mode.config.functionSet.setFunctionSet();
+              /*
+                D3:
+                - LOW  = 1 LINE DISPLAY
+                - HIGH = 2 LINE DISPLAY
+              */
+              this->pins.d3.value = LOW;
+            };
+            void setTo2() {
+              this->mode.config.setConfigMode();
+              this->mode.config.functionSet.setFunctionSet();
+              /*
+                D3:
+                - LOW  = 1 LINE DISPLAY
+                - HIGH = 2 LINE DISPLAY
+              */
+              this->pins.d3.value = HIGH;
+            };
+          } lines;
+          struct Dots {
+            void mode5x8() {
+              this->mode.config.setConfigMode();
+              this->mode.config.functionSet.setFunctionSet();
+              /*
+                D2:
+                - LOW  = 5x8 DOTS MODE
+                - HIGH = 5x11 DOTS MODE
+              */
+              this->pins.d2.value = LOW;
+            };
+            void mode5x11() {
+              this->mode.config.setConfigMode();
+              this->mode.config.functionSet.setFunctionSet();
+              /*
+                D2:
+                - LOW  = 5x8 DOTS MODE
+                - HIGH = 5x11 DOTS MODE
+              */
+              this->pins.d2.value = HIGH;
+            };
+          } dots;
+        } setNumberOf;
+      } functionSet;
 
-  //   } config;
-  // } mode;
+    } config;
+  } mode;
 };
 
 laaDisplay myDisplay(2, 3, 4, 5, 6, 7);
