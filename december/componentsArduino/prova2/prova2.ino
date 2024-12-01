28e6a5fb9ef7a7997de15fe1522b6d542e704f99
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-01 15:05:25 +0100

    SAVE

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -1,5 +1,3 @@
-#define LOW 0x0;
-
 struct PinStruct {
   int id, value;
 };
@@ -9,6 +7,37 @@ class laaDisplay {
   struct thisPins {
     PinStruct rs, en, d4, d5, d6, d7;
   } pins;
+  const int DELAY = 1;  // 300ns -> 0.0003ms -> 1ms
+
+  void sendCommand() {
+    this->pins.en.value = LOW;
+    digitalWrite(this->pins.en.id, this->pins.en.value);
+
+    this->pins.en.value = HIGH;
+    digitalWrite(this->pins.en.id, this->pins.en.value);
+    delay(this->DELAY);
+
+    this->pins.en.value = LOW;
+    digitalWrite(this->pins.en.id, this->pins.en.value);
+  };
+
+  void setConfigMode() {
+    this->pins.rs.value = LOW;  // LOW = CONFIG, HIGH = WRITING
+    this->pins.en.value = LOW;
+    this->pins.d4.value = LOW;
+    this->pins.d5.value = LOW;
+    this->pins.d6.value = LOW;
+    this->pins.d7.value = LOW;
+
+    digitalWrite(this->pins.rs.id, this->pins.rs.value);
+    digitalWrite(this->pins.en.id, this->pins.en.value);
+    digitalWrite(this->pins.d4.id, this->pins.d4.value);
+    digitalWrite(this->pins.d5.id, this->pins.d5.value);
+    digitalWrite(this->pins.d6.id, this->pins.d6.value);
+    digitalWrite(this->pins.d7.id, this->pins.d7.value);
+
+    sendCommand();
+  };
 
  public:
   laaDisplay(int rs, int en, int d4, int d5, int d6, int d7) {
@@ -19,7 +48,7 @@ class laaDisplay {
     this->pins.d6.id = d6;
     this->pins.d7.id = d7;
 
-    this->pins.en.value = LOW;
+    this->setConfigMode();
   };
 
   struct Mode {
@@ -38,8 +67,8 @@ class laaDisplay {
             void setToTwo() {};
           } lines;
           struct Dots {
-            void mode5x8 () {};
-            void mode5x11 () {};
+            void mode5x8() {};
+            void mode5x11() {};
           } dots;
         } setNumberOf;
       } functionSet;
@@ -47,10 +76,8 @@ class laaDisplay {
   } mode;
 };
 
-laaDisplay myDisplay(2,3,4,5,6,7);
+laaDisplay myDisplay(2, 3, 4, 5, 6, 7);
 
-void setup() {
-  myDisplay.mode.config.functionSet.bitMode.setTo4();
-};
+void setup() { myDisplay.mode.config.functionSet.bitMode.setTo4(); };
 
 void loop() {};
