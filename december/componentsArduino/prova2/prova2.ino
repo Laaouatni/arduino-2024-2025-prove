9e9208acb95f86e13ae295e7a09f8145d3694f62
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-01 17:41:40 +0100

    save

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -73,7 +73,10 @@ class laaDisplay {
  public:
   laaDisplay(int rs, int en, int d0, int d1, int d2, int d3, int d4, int d5, int d6, int d7) {
     _init();
-    this->configurations.functionSet.setBitMode()
+    
+    this->configurations.functionSet.setBitMode(true);
+    this->configurations.functionSet.setNumberOfLines(false);
+    this->configurations.functionSet.setDotMode(false);
 
     void _init() {
       this->pins.rs = {rs, LOW};
