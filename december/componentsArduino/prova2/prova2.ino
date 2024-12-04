54d2c111c10757523b92cd936299e8b403ead9d5
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-04 21:36:17 +0100

    save

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -2,27 +2,22 @@
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-struct PinStruct {
-  int id;
-  float value;
-};
-
 struct pins {
   struct fotocellula {
-    PinStruct ingresso  = {3};
-    PinStruct uscita    = {2};
+    int ingresso = 3;
+    int uscita = 2;
   };
-  PinStruct sensoreLux  = {A0};
-  PinStruct led         = {7};
+  int sensoreLux = A0;
+  int led = 7;
 };
 
 int numberOfPeople = 0;
 
 void setup() {
-  pinMode(pins.sensoreLux          .id, INPUT );
-  pinMode(pins.fotocellula.uscita  .id, INPUT );
-  pinMode(pins.fotocellula.ingresso.id, INPUT );
-  pinMode(pins.led                 .id, OUTPUT);
+  pinMode(pins.sensoreLux, INPUT);
+  pinMode(pins.fotocellula.ingresso, INPUT);
+  pinMode(pins.fotocellula.uscita, INPUT);
+  pinMode(pins.led, OUTPUT);
 
   lcd.init();
   lcd.backlight();
@@ -31,16 +26,25 @@ void setup() {
 };
 
 void loop() {
-  pins.sensoreLux          .value = analogRead (pins.sensoreLux          .id);
-  pins.fotocellula.ingresso.value = digitalRead(pins.fotocellula.ingresso.id);
-  pins.fotocellula.output  .value = digitalRead(pins.fotocellula.output  .id);
-
-  const bool isEntering = 
-
-  const bool isEmpty = numberOfPeople == 0;
+  const int livelloLux = analogRead(pins.sensoreLux);
+  const bool isEntering = digitalWrite(pins.fotocellula.ingresso);
+  const bool isExiting = digitalWrite(pins.fotocellula.uscita);
+  if (isEntering) numberOfPeople++;
   const bool isNight = pins.sensoreLux.value < 250;
-
+  bool isEmpty = numberOfPeople == 0;
+  if (isExiting && !isEmpty) numberOfPeople--;
+  isEmpty = numberOfPeople == 0;
   const bool canSystemTurnTheLight = !isEmpty && isNight;
-
   digitalWrite(pins.led.id, canSystemTurnTheLight);
+
+  int thisCursorY = 0;
+  String outputs[] = {"numero persone", String(numberOfPeople), "luminosita",
+                      String(livelloLux)};
+
+  lcd.clear();
+  for(auto thisString : outputs) {
+    lcd.print(thisString);
+    thisCursor = x++;
+    lcd.setCursor(0, thisCursor);
+  };
 }
