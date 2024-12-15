07c0ae44e6f79c06e001f1a83aee57d2f1bed519
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-15 23:38:25 +0100

    corrected typo in String type

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -3,44 +3,44 @@
 WiFiServer server(80);
 
 class laaClient {
- private:
-  String _request;
+  private:
+    String _request;
 
- public:
-  laaClient(String requestLineString) : _request(requestLineString) {};
-  void get(String filePath, void (&callback)()) {
-    bool isRequesting = _request.startsWith("GET /" + String(filePath));
-    if (!isRequesting) return;
-    callback();
-  };
-  void getDynamic(String filePath, void (&callback)()) {
-    const bool isContainingDynamicValueInTheFilePath =
-        _request.indexOf(":") != -1;
-
-    if (!isContainingDynamicValueInTheFilePath) {
+  public:
+    laaClient(String requestLineString) : _request(requestLineString) {};
+    void get(String filePath, void (&callback)()) {
       bool isRequesting = _request.startsWith("GET /" + String(filePath));
       if (!isRequesting) return;
       callback();
-      return;
-    }
+    };
+    void getDynamic(String filePath, void (&callback)()) {
+      const bool isContainingDynamicValueInTheFilePath =
+          _request.indexOf(":") != -1;
 
-    struct StringPartsToRemove {
-      const String start = "GET ";
-      const String end = "HTTP/1.1";
-    } stringPartsToRemove;
+      if (!isContainingDynamicValueInTheFilePath) {
+        bool isRequesting = _request.startsWith("GET /" + String(filePath));
+        if (!isRequesting) return;
+        callback();
+        return;
+      }
 
-    struct ModifiedStringWithout {
-      string withoutStarting;
-      string withoutEnding;
-      string withoutStartingAndEnding;
-    } modifiedRequestString;
+      struct StringPartsToRemove {
+        const String start = "GET ";
+        const String end = "HTTP/1.1";
+      } stringPartsToRemove;
 
-    modifiedRequestString.withoutEnding = _request.substring(
-        0, (_request.length() - 1) - (stringPartsToRemove.end.length() - 1));
-    modifiedRequestString.withoutStarting =
-        modifiedRequestString.withoutEnding.substring(
-            stringPartsToRemove.start.length - 1);
-  };
+      struct ModifiedStringWithout {
+        String withoutStarting;
+        String withoutEnding;
+        String withoutStartingAndEnding;
+      } modifiedRequestString;
+
+      modifiedRequestString.withoutEnding = _request.substring(
+          0, (_request.length() - 1) - (stringPartsToRemove.end.length() - 1));
+      modifiedRequestString.withoutStarting =
+          modifiedRequestString.withoutEnding.substring(
+              stringPartsToRemove.start.length - 1);
+    };
 };
 
 void setup() {
