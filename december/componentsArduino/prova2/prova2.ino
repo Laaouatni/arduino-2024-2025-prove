fd99a9f0aa5a5b23087698bb6f706b00b4afe6ca
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-15 17:52:26 +0100

    trying input

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -1,28 +1,26 @@
 struct Pins {
-  const int inputs[1] = { 5 };
+  const int inputs[19] = { 15,2,4,5,18,19,21,22,23,13,12,14,27,26,25,33,32,35,34 };
   const int outputs[19] = { 15,2,4,5,18,19,21,22,23,13,12,14,27,26,25,33,32,35,34 };
   // FUNZIONANO TUTTI TRANNE 35,34
 } pins;
 
 void setup() {
-  // for (auto thisPin : pins.inputs) {
-  //   pinMode(thisPin, INPUT);
-  //   Serial.print("I read from pin input n." + String(thisPin) +
-  //                "\t this value: " + String(analogRead(thisPin))
-  //               );
-  // };
-  for (auto thisPin : pins.outputs) {
-    pinMode(thisPin, OUTPUT);
+  for (auto thisPin : pins.inputs) {
+    pinMode(thisPin, INPUT);
   };
+  // for (auto thisPin : pins.outputs) {
+  //   pinMode(thisPin, OUTPUT);
+  // };
+  Serial.begin(115200);
 }
 
 void loop() {
-  for (auto thisPin : pins.outputs) {
-    digitalWrite(thisPin, HIGH);
+  for (auto thisPin : pins.inputs) {
+    const int value = analogRead(thisPin);
+    if(value != 0) {
+      Serial.println("I read from pin input n." + String(thisPin) +
+                 "\t this value: " + String(analogRead(thisPin))
+                );
+    }
   };
-  delay(1000);
-  for (auto thisPin : pins.outputs) {
-    digitalWrite(thisPin, LOW);
-  };
-  delay(1000);
 };
