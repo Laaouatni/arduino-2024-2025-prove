ef12fc4b64eea809db0b5282ce0ace7e56a2533d
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-15 18:38:06 +0100

    provo a leggere dal monitor usando serial per controllare ogni pin senza compilare ogni volta

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -1,7 +1,5 @@
 const int inputs[19] = { 15, 2, 4, 5, 18, 19, 21, 22, 23, 13, 12, 14, 27, 26, 25, 33, 32, 35, 34 };
-const int myPinInput = inputs[0];
-
-// PROBLEMA FATTO CON RESISTENZA DI PULL_DOWN
+int myPinInput = inputs[0];
 
 void setup() {
   pinMode(myPinInput, INPUT);
@@ -10,8 +8,11 @@ void setup() {
 }
 
 void loop() {
+  if(Serial.available() > 0) {
+    myPinInput = String(Serial.readStringUntil('\n')).toInt();
+  };
   const int value = analogRead(myPinInput);
   if (value != 0) {
-    Serial.println("I read from pin input n." + String(myPinInput) + "\t this value: " + String(analogRead(myPinInput)));
+    Serial.println("I read from pin input n." + String(myPinInput) + "\t this value: " + String(value));
   };
 };
