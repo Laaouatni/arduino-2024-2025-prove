3df2633a90a0fd814948b07e12a8f96f4c80f6c2
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-15 17:01:51 +0100

    save

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -1,62 +1,19 @@
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-
-const int NIGHT_MAX_LUX = 250;
-
 struct Pins {
-  struct Fotocellula {
-    int ingresso = 3;
-    int uscita = 2;
-  } fotocellula;
-  int sensoreLux = A0;
-  int led = 7;
+  int inputs[] = {5};
+  int outputs[] = {5};
 } pins;
 
-int numberOfPeople = 0;
-bool isEmpty() { return numberOfPeople == 0; };
-bool hasEntered, hasExited;
-
 void setup() {
-  pinMode(pins.sensoreLux          , INPUT);
-  pinMode(pins.fotocellula.ingresso, INPUT);
-  pinMode(pins.fotocellula.uscita  , INPUT);
-  pinMode(pins.led                 , OUTPUT);
-
-  lcd.init();
-  lcd.backlight();
-
-  Serial.begin(9600);
-};
-
-void loop() {
-  const int livelloLux  = analogRead(pins.sensoreLux);
-
-  const bool isEntering = digitalWrite(pins.fotocellula.ingresso);
-  const bool isExiting  = digitalWrite(pins.fotocellula.uscita);
-
-  if (isEntering != hasEntered)                numberOfPeople++;
-  if ((isExiting && !isEmpty()) != hasExiting) numberOfPeople--;
-
-  const bool isNight = pins.sensoreLux.value < NIGHT_MAX_LUX;
-  const bool canSystemTurnTheLight = isNight && !isEmpty();
-
-  digitalWrite(pins.led.id, canSystemTurnTheLight);
-
-  String outputs[] = {
-    "numero persone", String(numberOfPeople), 
-    "luminosita"    , String(livelloLux)
+  // for (auto thisPin : pins.inputs) {
+  //   pinMode(thisPin, INPUT);
+  //   Serial.print("I read from pin input n." + String(thisPin) +
+  //                "\t this value: " + String(analogRead(thisPin))
+  //               );
+  // };
+  for (auto thisPin : pins.outputs) {
+    pinMode(thisPin, OUTPUT);
+    digitalWrite(thisPin, HIGH);
   };
-  int thisCursorY = 0;
-
-  lcd.clear();
-  for(auto thisString : outputs) {
-    lcd.print(thisString);
-    Serial.print(String(thisString) + "\t");
-    lcd.setCursor(0, thisCursorY++);
-  };
-  Serial.println(" ");
-
-  hasEntered = isEntering;
-  hasExited = isExiting;
 }
+
+void loop() {};
