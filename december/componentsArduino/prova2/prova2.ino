864e512f862fef9a5875937f6fa0580d8fb063c0
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-15 18:04:20 +0100

    provo per ogni pin e metto una resistenza alta

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -1,25 +1,15 @@
-struct Pins {
-  const int inputs[19] = { 18,19,21,22,23,12,27,26,25,33,32,35,34 };
-  const int outputs[19] = { 15,2,4,5,18,19,21,22,23,13,12,14,27,26,25,33,32,35,34 };
-} pins;
+const int inputs[19] = { 15, 2, 4, 5, 18, 19, 21, 22, 23, 13, 12, 14, 27, 26, 25, 33, 32, 35, 34 };
+const int myPinInput = inputs[0];
 
 void setup() {
-  for (auto thisPin : pins.inputs) {
-    pinMode(thisPin, INPUT);
-  };
-  // for (auto thisPin : pins.outputs) {
-  //   pinMode(thisPin, OUTPUT);
-  // };
+  pinMode(myPinInput, INPUT);
   Serial.begin(115200);
+  Serial.print("pinMode: " + String(myPinInput) + " INPUT");
 }
 
 void loop() {
-  for (auto thisPin : pins.inputs) {
-    const int value = analogRead(thisPin);
-    if(value != 0) {
-      Serial.println("I read from pin input n." + String(thisPin) +
-                 "\t this value: " + String(analogRead(thisPin))
-                );
-    }
+  const int value = analogRead(thisPin);
+  if (value != 0) {
+    Serial.println("I read from pin input n." + String(thisPin) + "\t this value: " + String(analogRead(thisPin)));
   };
 };
