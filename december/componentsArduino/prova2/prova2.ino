577f5e822583e033f016b8cfb700f4f5c7d48fab
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-16 20:51:21 +0100

    save test that for now don't work. post

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -1,3 +1,6 @@
+// c:\Users\laaou\Documents\Arduino\libraries\ESPAsyncWebServer\src\WebAuthentication.cpp
+// change mbedtls_md5_starts_ret to mbedtls_md5_starts
+
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 
@@ -12,7 +15,8 @@ void setup() {
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   server.on("/digitalRead", HTTP_POST, [](AsyncWebServerRequest *req) {
-    req->send(200, "application/json", "ciao");
+    Serial.println("post");
+    req->send(200, "application/json", "{\"idk\":\"2\"");
   });
 
   pinMode(5, OUTPUT);
