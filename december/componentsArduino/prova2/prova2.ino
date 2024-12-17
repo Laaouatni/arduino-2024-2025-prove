305f2ddbb50c66ad1bda7d834eb27d44e5d6b7a7
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-17 23:13:10 +0100

    finalmente la logica splitting funziona

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -15,48 +15,32 @@ void setup() {
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   server.onNotFound([](AsyncWebServerRequest* request) {
-    String urlPath = "HELLO";
+    String urlPath = request->url();
     String valueToReturn;
 
-    const int MAX_SPLITTED_PARTS = 5;
+    const int MAX_SPLITTED_PARTS = 3;
     int thisStartIndex = 0+1;
     int numberOfFoundedParts = 0;
 
-    String foundedSplittedParts[MAX_SPLITTED_PARTS] = {};
+    String foundedSplittedParts[MAX_SPLITTED_PARTS];
 
-    Serial.println(foundedSplittedParts[5]);
+    for (int charIndex = thisStartIndex; charIndex < urlPath.length(); charIndex++) {
+      const char thisChar = urlPath[charIndex];
+      const bool isLastCharIteration = charIndex == urlPath.length()-1;
+      const bool isLastAvailablePartInArray = numberOfFoundedParts == MAX_SPLITTED_PARTS;
+      const bool isThisCharSlash = thisChar == '/';
+      if (isLastAvailablePartInArray || isLastCharIteration) {
+        const int choosedEndIndex = isLastCharIteration && isThisCharSlash ? urlPath.length()-1 : urlPath.length();
+        foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex, choosedEndIndex);
+        break;
+      };
+      if (!isThisCharSlash) continue;
+      foundedSplittedParts[numberOfFoundedParts] =urlPath.substring(thisStartIndex, charIndex);
+      numberOfFoundedParts++;
+      thisStartIndex = charIndex+1;
+    };
 
-    // for (int charIndex = thisStartIndex; charIndex < urlPath.length(); charIndex++) {
-    //   const char thisChar = urlPath[charIndex];
-    //   const bool isLastCharIteration = charIndex == urlPath.length()-1;
-    //   const bool isLastAvailablePartInArray = numberOfFoundedParts == MAX_SPLITTED_PARTS;
-    //   const bool isThisCharSlash = thisChar == '/';
-    //     // Serial.println("BEFOREIF: " + String(numberOfFoundedParts));
-    //   if(isLastAvailablePartInArray) {
-    //     foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex);
-    //     break;
-    //   };
-    //   // if(isLastCharIteration) {
-    //   //   founded
-    //   // }
-    //   // if (isLastAvailablePartInArray || isLastCharIteration) {
-    //   //   Serial.println("PRIMOIF: " + String(numberOfFoundedParts));
-    //   //   const int choosedEndIndex = isLastCharIteration && isThisCharSlash ? urlPath.length()-1 : urlPath.length();
-    //   //   foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex, choosedEndIndex);
-    //   //   break;
-    //   // };
-    //   if (!isThisCharSlash) continue;
-    //   // Serial.println("NORMAL: " + String(numberOfFoundedParts) + "\t" + urlPath.substring(thisStartIndex, charIndex));
-  
-
-    //   foundedSplittedParts[numberOfFoundedParts] =
-    //       urlPath.substring(thisStartIndex, charIndex);
-      
-    //   numberOfFoundedParts++;
-    //   thisStartIndex = charIndex + 1;
-    // };
-
-    for (int i = 0; i <= MAX_SPLITTED_PARTS; i++) {  // Print the 3 parts
+    for (int i = 0; i < MAX_SPLITTED_PARTS; i++) {
       Serial.println(String(i) + foundedSplittedParts[i]);
     }
 
