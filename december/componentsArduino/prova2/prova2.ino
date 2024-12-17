65fb04bc64d2a9b86d13dd4a3a8f021e398fc92f
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-17 22:53:53 +0100

    I found that the bug is always avaiable even without loop, but he get the urlPath from somewhere

--- december/componentsArduino/prova2/prova2.ino
+++ december/componentsArduino/prova2/prova2.ino
@@ -15,46 +15,46 @@ void setup() {
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   server.onNotFound([](AsyncWebServerRequest* request) {
-    String urlPath = request->url();
+    String urlPath = "HELLO";
     String valueToReturn;
 
     const int MAX_SPLITTED_PARTS = 5;
     int thisStartIndex = 0+1;
     int numberOfFoundedParts = 0;
 
-    String foundedSplittedParts[MAX_SPLITTED_PARTS];
+    String foundedSplittedParts[MAX_SPLITTED_PARTS] = {};
 
-    for (int charIndex = thisStartIndex; charIndex < urlPath.length(); charIndex++) {
-      const char thisChar = urlPath[charIndex];
-      const bool isLastCharIteration = charIndex == urlPath.length()-1;
-      const bool isLastAvailablePartInArray = numberOfFoundedParts == MAX_SPLITTED_PARTS;
-      const bool isThisCharSlash = thisChar == '/';
-        Serial.println("BEFOREIF: " + String(numberOfFoundedParts));
-      if(isLastAvailablePartInArray) {
-        foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex);
-        break;
-      };
-      // if(isLastCharIteration) {
-      //   founded
-      // }
-      // if (isLastAvailablePartInArray || isLastCharIteration) {
-      //   Serial.println("PRIMOIF: " + String(numberOfFoundedParts));
-      //   const int choosedEndIndex = isLastCharIteration && isThisCharSlash ? urlPath.length()-1 : urlPath.length();
-      //   foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex, choosedEndIndex);
-      //   break;
-      // };
-      if (!isThisCharSlash) continue;
-      Serial.println("NORMAL: " + String(numberOfFoundedParts) + "\t" + urlPath.substring(thisStartIndex, charIndex));
+    Serial.println(foundedSplittedParts[5]);
 
-      if(thisStartIndex != charIndex) {
+    // for (int charIndex = thisStartIndex; charIndex < urlPath.length(); charIndex++) {
+    //   const char thisChar = urlPath[charIndex];
+    //   const bool isLastCharIteration = charIndex == urlPath.length()-1;
+    //   const bool isLastAvailablePartInArray = numberOfFoundedParts == MAX_SPLITTED_PARTS;
+    //   const bool isThisCharSlash = thisChar == '/';
+    //     // Serial.println("BEFOREIF: " + String(numberOfFoundedParts));
+    //   if(isLastAvailablePartInArray) {
+    //     foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex);
+    //     break;
+    //   };
+    //   // if(isLastCharIteration) {
+    //   //   founded
+    //   // }
+    //   // if (isLastAvailablePartInArray || isLastCharIteration) {
+    //   //   Serial.println("PRIMOIF: " + String(numberOfFoundedParts));
+    //   //   const int choosedEndIndex = isLastCharIteration && isThisCharSlash ? urlPath.length()-1 : urlPath.length();
+    //   //   foundedSplittedParts[numberOfFoundedParts] = urlPath.substring(thisStartIndex, choosedEndIndex);
+    //   //   break;
+    //   // };
+    //   if (!isThisCharSlash) continue;
+    //   // Serial.println("NORMAL: " + String(numberOfFoundedParts) + "\t" + urlPath.substring(thisStartIndex, charIndex));
+  
 
-      foundedSplittedParts[numberOfFoundedParts] =
-          urlPath.substring(thisStartIndex, charIndex);
-      }
-
-      numberOfFoundedParts++;
-      thisStartIndex = charIndex + 1;
-    };
+    //   foundedSplittedParts[numberOfFoundedParts] =
+    //       urlPath.substring(thisStartIndex, charIndex);
+      
+    //   numberOfFoundedParts++;
+    //   thisStartIndex = charIndex + 1;
+    // };
 
     for (int i = 0; i <= MAX_SPLITTED_PARTS; i++) {  // Print the 3 parts
       Serial.println(String(i) + foundedSplittedParts[i]);
