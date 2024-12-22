7deded4a9244cbe31f5d65adbb8e59517421facf
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-22 23:15:36 +0100

    it work great maximum 20ms delay in setInterval, then after like 1 minute it cant handle that, and write ERROR: Too many messages queued

--- 2025/WebSocket/prova22-12-2024/backend/backend.ino
+++ 2025/WebSocket/prova22-12-2024/backend/backend.ino
@@ -16,15 +16,11 @@ void setup() {
                 AwsEventType type, void *arg, uint8_t *data, size_t len) {
     const bool isConnecting = type == WS_EVT_CONNECT;
     const bool isDisconnecting = type == WS_EVT_DISCONNECT;
-    // const bool hasError = type == WS_EVT_ERROR;
     const bool hasData = type == WS_EVT_DATA;
 
-    if (isConnecting)
-      Serial.println("WebSocket Client Connected! IP Address: " +
-                     client->remoteIP().toString());
-    if (isDisconnecting)
-      Serial.println("WebSocket Client Disconnected! IP Address: " +
-                     client->remoteIP().toString());
+    if (isConnecting)     Serial.println("WebSocket Client Connected! IP Address: " + client->remoteIP().toString());
+    if (isDisconnecting)  Serial.println("WebSocket Client Disconnected! IP Address: " + client->remoteIP().toString());
+    
     if (!hasData) return;
 
     ws.textAll("Hello from ESP32 textall");
