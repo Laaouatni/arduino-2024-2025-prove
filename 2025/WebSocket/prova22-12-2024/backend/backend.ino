6c617091e93cbb2155c78392b61dc3a07a8f6429
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-22 22:04:36 +0100

    save prova, websocket manda allo stesso sito. mentre non riesco a ottenere remoteIP da esp32 a causa di vari errori di compilazione

--- 2025/WebSocket/prova22-12-2024/backend/backend.ino
+++ 2025/WebSocket/prova22-12-2024/backend/backend.ino
@@ -1,28 +1,35 @@
-#include <WiFi.h>
+#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <WiFi.h>
 
 AsyncWebServer server(80);
-AsyncWebSocket ws();
+AsyncWebSocket ws("/ws");
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin("nomeWifi", "passwordWifi");
-  while (WiFi.status() != WL_CONNECTED) {};
+  while (WiFi.status() != WL_CONNECTED) {
+  };
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
 
   server.onNotFound([](AsyncWebServerRequest *request) {
-    Serial.println("server client ip", request->remoteIP().toString());
+    Serial.println("server client ip", request->client()->remoteIP().toString());
     request->send(200, "text/plain", "web server call");
   });
 
-  ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
+  ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client,
+                AwsEventType type, void *arg, uint8_t *data, size_t len) {
     const bool isConnecting = type == WS_EVT_CONNECT;
     const bool isDisconnecting = type == WS_EVT_DISCONNECT;
     // const bool hasError = type == WS_EVT_ERROR;
     const bool hasData = type == WS_EVT_DATA;
 
-    if(isConnecting)    Serial.println("WebSocket Client Connected! IP Address: "    + client.remoteIP().toString());
-    if(isDisconnecting) Serial.println("WebSocket Client Disconnected! IP Address: " + client.remoteIP().toString());
+    if (isConnecting)
+      Serial.println("WebSocket Client Connected! IP Address: " +
+                     client->remoteIP().toString());
+    if (isDisconnecting)
+      Serial.println("WebSocket Client Disconnected! IP Address: " +
+                     client->remoteIP().toString());
     if (!hasData) return;
 
     ws.textAll("Hello from ESP32 textall");
@@ -32,6 +39,4 @@ void setup() {
   server.begin();
 }
 
-void loop() {
-  ws.cleanupClients();
-};
+void loop() { ws.cleanupClients(); };
