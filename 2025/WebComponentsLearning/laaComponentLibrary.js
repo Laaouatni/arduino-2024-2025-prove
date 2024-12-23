6193f99329bc615c3ef89f2f64ecc6ddec4ec311
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-24 00:55:50 +0100

    prove non funzionanti

--- 2025/WebComponentsLearning/laaComponentLibrary.js
+++ 2025/WebComponentsLearning/laaComponentLibrary.js
@@ -2,22 +2,39 @@ document.querySelectorAll("template").forEach((template) => {
   class ThisComponent extends HTMLElement {
     constructor() {
       super();
-      let shadow = this.attachShadow({ mode: "open" });
+      const shadow = this.attachShadow({ mode: "open" });
       shadow.appendChild(template.content.cloneNode(true));
-    }
 
+      // Execute custom logic from the <script> tag inside the template
+      const script = shadow.querySelector("script");
+      if (script) {
+        const customLogic = new Function(
+          "attributeChangedAttributes",
+          script.textContent
+        );
+        customLogic.call(this, this._setupCustomAttributeCallback.bind(this));
+      }
+    }
+    
     static get observedAttributes() {
-      const attributesToObserve = [...template.attributes].filter((thisAttribute) => {
-        const isIdAttribute = thisAttribute.nodeName === "id";
-        if (isIdAttribute) return;
-        return thisAttribute.nodeName;
-      });
-      return attributesToObserve
+      // Dynamically build observed attributes from the template's attributes
+      return [...template.attributes]
+      .map((attr) => attr.nodeName)
+      .filter((attrName) => attrName !== "id");
     }
-
+    
+    _setupCustomAttributeCallback(callback) {
+      this._customAttributeCallback = callback;
+    }
+    
     attributeChangedCallback(attributeName, oldValue, newValue) {
-      // console.log({ attributeName, oldValue, newValue });
+      console.log(this._customAttributeCallback);
+      if (this._customAttributeCallback) {
+        this._customAttributeCallback(attributeName, oldValue, newValue);
+      }
     }
   }
+
+  // Define the custom element
   customElements.define(template.id, ThisComponent);
 });
