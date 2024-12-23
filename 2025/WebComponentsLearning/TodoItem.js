0a82ca019581ed16c61c3b53b78f8c5ac8d31c88
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-23 23:07:05 +0100

    adesso posso scrivere <template> per creare componenti con style propri tramite shadowdom con un id e direttamente usarlo senza usare javascript, tramite un script js forEach

--- 2025/WebComponentsLearning/TodoItem.js
+++ 2025/WebComponentsLearning/TodoItem.js
@@ -1,9 +1,21 @@
-class TodoItem extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.appendChild(document.getElementById("template-ciao").content.cloneNode(true));
+document.querySelectorAll("template").forEach((template) => {
+  class ThisComponent extends HTMLElement {
+    constructor() {
+      super();
+      const shadow = this.attachShadow({ mode: "open" });
+      shadow.appendChild(template.content.cloneNode(true));
+    }
   }
-}
+  customElements.define(template.id, ThisComponent);
+});
 
-customElements.define("todo-item", TodoItem);
+
+// class TodoItem extends HTMLElement {
+//   constructor() {
+//     super();
+//     const shadow = this.attachShadow({ mode: "open" });
+//     shadow.appendChild(document.getElementById("template-ciao").content.cloneNode(true));
+//   }
+// }
+
+// customElements.define("todo-item", TodoItem);
