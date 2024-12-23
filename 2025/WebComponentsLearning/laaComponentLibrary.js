fe0430fdbdf1a1dbdc2589dbfe4f1e452ffbd959
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-23 23:32:54 +0100

    trying to see get the specific attribute of this component element

--- 2025/WebComponentsLearning/laaComponentLibrary.js
+++ 2025/WebComponentsLearning/laaComponentLibrary.js
@@ -8,14 +8,3 @@ document.querySelectorAll("template").forEach((template) => {
   }
   customElements.define(template.id, ThisComponent);
 });
-
-
-// class TodoItem extends HTMLElement {
-//   constructor() {
-//     super();
-//     const shadow = this.attachShadow({ mode: "open" });
-//     shadow.appendChild(document.getElementById("template-ciao").content.cloneNode(true));
-//   }
-// }
-
-// customElements.define("todo-item", TodoItem);
