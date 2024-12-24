33dae93128e6ef16bcfc4186945967c99bc026ff
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-24 16:22:38 +0100

    save

---
+++ 2025/WebComponentsLearning/server.ino
@@ -0,0 +1,42 @@
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <WiFi.h>
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin("nomeWifi", "passwordWifi");
+  while (WiFi.status() != WL_CONNECTED) {
+  };
+  Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
+
+  ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client,
+                AwsEventType type, void *arg, uint8_t *data, size_t len) {
+    const bool isConnecting = type == WS_EVT_CONNECT;
+    const bool isDisconnecting = type == WS_EVT_DISCONNECT;
+    const bool hasData = type == WS_EVT_DATA;
+
+    if (isConnecting)     Serial.println("WebSocket Client Connected! IP Address: " + client->remoteIP().toString());
+    if (isDisconnecting)  Serial.println("WebSocket Client Disconnected! IP Address: " + client->remoteIP().toString());
+    
+    if (!hasData) return;
+
+    Serial.println("data: " + String(*data) + "\t ((char*)data): " + String((char*)data) + "\t len: " + String(len) + "\t string.substring(0, len): " + String((char*)data).substring(0, len));
+
+    ws.textAll("Hello from ESP32 textall " + String((char*)data).substring(0, len));
+  });
+
+  server.onNotFound([](AsyncWebServerRequest *request) {
+    Serial.println("Server Client onNotFound IP Address: " + request->client()->remoteIP().toString());
+    request->send(200, "text/plain", "web server call");
+  });
+
+  server.addHandler(&ws);
+  server.begin();
+}
+
+void loop() { 
+  ws.cleanupClients(); 
+};
