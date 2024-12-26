8277934a31317019bbf54e76d6f32fd28071149f
Laaouatni Anas <laaouatni.anas@outlook.it>
2024-12-26 12:16:48 +0100

    starting new project to improve my web component library

---
+++ 2025/svelteRecreation/laaWebComponentLibrary.js
@@ -0,0 +1,81 @@
+document.querySelectorAll("template").forEach((thisTemplateElement) => {
+  class ThisComponent extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    static get observedAttributes() {
+      const attributesToObserve = [...thisTemplateElement.attributes]
+        .map((thisAttribute) => thisAttribute.nodeName)
+        .filter((thisAttribute) => {
+          let canSkip = false;
+          ["id", "class"].forEach((thisAttributeName) => {
+            if (thisAttribute == thisAttributeName) canSkip = true;
+          });
+          return !canSkip;
+        });
+      return attributesToObserve;
+    }
+
+    _connectedCallback() {}
+    _disconnectedCallback() {}
+    /**
+     * @param {string} attributeName
+     * @param {any} oldValue
+     * @param {any} newValue
+     */
+    _attributeChangedCallback(attributeName, oldValue, newValue) {}
+
+    connectedCallback() {
+      this._connectedCallback();
+      ["class", "style"].forEach((thisAttributeName) => {
+        if (thisTemplateElement.hasAttribute(thisAttributeName)) {
+          this.setAttribute(
+            thisAttributeName,
+            `${
+              this.getAttribute(thisAttributeName) || ""
+            } ${thisTemplateElement.getAttribute(thisAttributeName)}`.trim(),
+          );
+        }
+      });
+
+      const allScriptElementsInsideTemplate =
+        thisTemplateElement.content.querySelectorAll("script");
+
+      allScriptElementsInsideTemplate.forEach((thisScriptTemplateElement) => {
+        const generatedScriptElementInsideComponent =
+          document.createElement("script");
+        generatedScriptElementInsideComponent.textContent =
+          isolateScriptStringInsideComponent(
+            thisScriptTemplateElement.textContent || "",
+          );
+        this.appendChild(generatedScriptElementInsideComponent);
+      });
+
+      /**
+       * @param   {string} thisScriptString
+       * @returns {string}
+       */
+      function isolateScriptStringInsideComponent(thisScriptString) {
+        return `(()=>{
+          const thisComponent = document.currentScript.parentElement;
+          ${thisScriptString}}
+        )()`;
+      }
+    }
+
+    disconnectedCallback() {
+      this._disconnectedCallback();
+    }
+    /**
+     * @param {string} attributeName
+     * @param {any} oldValue
+     * @param {any} newValue
+     */
+    attributeChangedCallback(attributeName, oldValue, newValue) {
+      this._attributeChangedCallback(attributeName, oldValue, newValue);
+    }
+  }
+
+  customElements.define(thisTemplateElement.id, ThisComponent);
+});
